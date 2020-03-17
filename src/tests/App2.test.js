f51cb3308db2aa73daff675e80a3f335dd470f71
fernfernfern <fernandomouco@gmail.com>
2020-03-17 20:19:05 +0000

    wheeee

--- src/tests/App2.test.js
+++ src/tests/App2.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import App from '../App'
+import sleep from '../tools'
 
 it('changes the text on click', () => {
   const wrapper = shallow(<App/>)
   let text = wrapper.find('.text-container')
-
+  sleep(10)
   expect(text.exists()).toBe(true)
   expect(wrapper.state('words')).toBe('')
   wrapper.find('button').simulate('click')
