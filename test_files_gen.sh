cd48cb86030561dd9ffeba8e417d2f0036224896
fernfernfern <fernandomouco@gmail.com>
2020-03-18 14:43:02 +0000

    again

--- test_files_gen.sh
+++ test_files_gen.sh
@@ -10,11 +10,15 @@ END
 )
 
 TEST=$(cat <<-END
-import React from 'react'
-import { shallow } from 'enzyme'
-import App from '../App'
-import { sleep, defaultDuration } from '../tools'
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
+it('changes the text on click', () => {
+  const wrapper = shallow(<App/>)
+  let text = wrapper.find('.text-container')
+  expect(text.exists()).toBe(true)
+  expect(wrapper.state('words')).toBe('')
+  wrapper.find('button').simulate('click')
+  text = wrapper.find('.text-container')
+  expect(text.text()).toBe(wrapper.state('words'))
+})
 END
 )
 
@@ -22,9 +26,8 @@ for i in {0..2000}
 do
   echo "$HEAD" > "./src/tests/App$(printf "%03d" "$i").test.js"
 
-for l in {0..100}
-do
-  echo "$TEST" >> "./src/tests/App$(printf "%03d" "$i").test.js"
-done
-
+  for l in {0..100}
+  do
+    echo "$TEST" >> "./src/tests/App$(printf "%03d" "$i").test.js"
+  done
 done
