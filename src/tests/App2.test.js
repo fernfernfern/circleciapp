0672bcf9894a178cdfaa1335010437504c4a27b4
fernfernfern <fernandomouco@gmail.com>
2020-03-18 09:25:31 +0000

    test

--- src/tests/App2.test.js
+++ src/tests/App2.test.js
@@ -5,13 +5,14 @@ import { sleep, defaultDuration } from '../tools'
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
 
-it('changes the text on click', async () => {
+it('changes the text on click', () => {
   const wrapper = shallow(<App/>)
   let text = wrapper.find('.text-container')
-  await expect(sleep(defaultDuration)).resolves.toBeUndefined()
+  expect(sleep(defaultDuration)).resolves.toBeUndefined()
   expect(text.exists()).toBe(true)
   expect(wrapper.state('words')).toBe('')
   wrapper.find('button').simulate('click')
   text = wrapper.find('.text-container')
   expect(text.text()).toBe(wrapper.state('words'))
 })
+
--- src/tests/App4.test.js
+++ src/tests/App4.test.js
@@ -5,10 +5,10 @@ import { sleep, defaultDuration } from '../tools'
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
 
-it('changes the text on click', async () => {
+it('changes the text on click', () => {
   const wrapper = shallow(<App/>)
   let text = wrapper.find('.text-container')
-  await expect(sleep(defaultDuration)).resolves.toBeUndefine()
+  expect(sleep(defaultDuration)).resolves.toBeUndefined()
   expect(text.exists()).toBe(true)
   expect(wrapper.state('words')).toBe('')
   wrapper.find('button').simulate('click')
